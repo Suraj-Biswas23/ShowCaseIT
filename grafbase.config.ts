f26b004730930e76031daad30928ad7dc95b0924
2347259-Suraj-Biswas <suraj.biswas@mca.christuniversity.in>
2024-09-06 13:16:45 +0530

    made changes to model in grafbase.config

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,43 +1,50 @@
-// import { graph, config, auth } from '@grafbase/sdk';
+import { graph, config, auth, connector } from '@grafbase/sdk';
 
-// const g = graph.Standalone()
+const g = graph.Standalone()
 
-// // @ts-ignore
-// const User = g.model('User', {
-//   name: g.string().length({ min: 2, max: 100 }),
-//   email: g.string().unique(),
-//   avatarUrl: g.url(),
-//   description: g.string().length({ min: 2, max: 1000 }).optional(),
-//   githubUrl: g.url().optional(),
-//   linkedinUrl: g.url().optional(), 
-//   projects: g.relation(() => Project).list().optional(),
-// }).auth((rules) => {
-//   rules.public().read()
-// })
+const mongodb = connector.MongoDB('MongoDB', {
+  url: g.env('MONGODB_URL'),
+  apiKey: g.env('MONGODB_API_KEY'),
+  dataSource: g.env('MONGODB_DATA_SOURCE'),
+  database: g.env('MONGODB_DATABASE'),
+})
 
-// // @ts-ignore
-// const Project = g.model('Project', {
-//   title: g.string().length({ min: 3 }),
-//   description: g.string(), 
-//   image: g.url(),
-//   liveSiteUrl: g.url(), 
-//   githubUrl: g.url(), 
-//   category: g.string().search(),
-//   createdBy: g.relation(() => User),
-// }).auth((rules) => {
-//   rules.public().read()
-//   rules.private().create().delete().update()
-// })
+// Define the models directly
+const User = mongodb.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.ref('Project').list().optional(),
+})
+.collection('users')
+.auth(rules => rules.public().read())
 
-// const jwt = auth.JWT({
-//   issuer: 'grafbase',
-//   secret:  g.env('NEXTAUTH_SECRET')
-// })
+const Project = mongodb.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string(),
+  createdBy: g.ref('User'),
+})
+.auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+})
 
-// export default config({
-//   schema: g,
-//   auth: {
-//     providers: [jwt],
-//     rules: (rules) => rules.private()
-//   },
-// })
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
+})
+
+export default config({
+  graph: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  },
+})
